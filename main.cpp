77fa4220618c71d92693b391db724fd68c96598c
shenchunyang <703958897@qq.com>
2015-12-03 11:13:55 +0800

    Linked list
    定义一个结构体，包含name、age和student类型的next指针。定义一个int变量i，和student类型的指针变量p、q、head；新
    建的结构体地址赋值给head指针变量，并将head指针所指向的结构体中的next指针域赋值为0.再用p指针也指向student结构体。
    在循环体中，将新建的结构体地址赋值给q，并将q赋值给指针变量p所指向的结构体next指针域，再对q变量所指向的next初始化。
    将输入的name、age存放在q指向的地址下面，并将p再初始化。
    head所指的next指针域赋值给p；当p存放地址时（即p！＝0），输出p地址所存储的name、age。

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  Linked list
+//
+//  Created by ry耷拉拉星球 on 15/12/2.
+//  Copyright (c) 2015年 ry耷拉拉星球. All rights reserved.
+//
+
+#include <iostream>
+struct student
+{
+    char name[100];
+    int age;
+    student *next;
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int i;
+    student *p,*q,*head;
+    head=new student;
+    head->next=0;
+    p=head;
+    for(i=0;i<4;i++)
+    {
+        q=new student;
+        p->next=q;
+        q->next=0;
+        scanf("%s %d",q->name,&(q->age));
+        p=q;
+    }
+    p=head->next;
+    while(p!=0)
+    {
+        printf("%s %d",p->name,p->age);
+        p=p->next;
+    }
+    
+    return 0;
+}
